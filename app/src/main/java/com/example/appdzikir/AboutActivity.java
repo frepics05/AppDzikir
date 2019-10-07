50b2833c880c45a5a0e8043668269c1e93b632dc
Fadhlan <49526938+MuhammadFadhlanWahyudi@users.noreply.github.com>
2019-10-07 14:58:48 +0700

    Update AboutActivity.java
    Error Gpp y

--- app/src/main/java/com/example/appdzikir/AboutActivity.java
+++ app/src/main/java/com/example/appdzikir/AboutActivity.java
@@ -11,6 +11,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+                getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 
     public void Pindah6(View view) {
